c05357466019f9254ffc10f338ebc7f0c4b6f7c6
AadeshGupta10 <106733380+AadeshGupta10@users.noreply.github.com>
2025-01-15 23:43:47 +0530

    Changes in Hash Comparison

--- Backend/utilities/hash.js
+++ Backend/utilities/hash.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { createHmac } = require("crypto")
 
 const hash_generation = (item) => {
-    return (createHmac("sha256", process.env.SECRET_CODE).update((item).toString()).digest("hex")).toString();
+    return JSON.stringify(createHmac("sha256", process.env.SECRET_CODE).update((item).toString()).digest("hex"));
 }
 
 const hash_verification = (current, old_hash) => {
-    return hash_generation(current) === old_hash;
+    return hash_generation(current) === (old_hash);
 }
 
 module.exports = {
